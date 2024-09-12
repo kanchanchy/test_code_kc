b77e1b5a3b0d82151cf55e849e3858f4949f98a5
kanchanchy <kanchanchy09@gmail.com>
2024-09-11 21:53:37 -0600

    'code-changed'

--- FraudDetectionTest.cpp
+++ FraudDetectionTest.cpp
@@ -1261,7 +1261,7 @@ void FraudDetectionTest::testingWithRealData(int numDataSplits, int dataBatchSiz
                          .values({orderRowVector})
                          .project({"o_customer_sk", "o_order_id", "date_to_timestamp_1(o_date) AS o_timestamp"})
                          .filter("o_timestamp IS NOT NULL")
-                         .filter("is_weekday(o_timestamp) = 1")
+                         //.filter("is_weekday(o_timestamp) = 1")
                          .singleAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
                          .hashJoin({"o_customer_sk"},
                              {"t_sender"},
