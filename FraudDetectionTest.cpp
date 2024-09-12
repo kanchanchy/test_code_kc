69d45c27714744a0aa06bd3c5a46cacffd85f9fd
kanchanchy <kanchanchy09@gmail.com>
2024-09-12 14:20:23 -0600

    'model-changed'

--- FraudDetectionTest.cpp
+++ FraudDetectionTest.cpp
@@ -1308,7 +1308,7 @@ void FraudDetectionTest::testingWithRealData(int numDataSplits, int dataBatchSiz
                          //.localPartition({"o_customer_sk"})
                          //.finalAggregation()
                          .singleAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
-                         .hashJoin({"o_customer_sk"},
+                         /*.hashJoin({"o_customer_sk"},
                              {"t_sender"},
                              exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
                              .values({transactionRowVector})
@@ -1339,7 +1339,7 @@ void FraudDetectionTest::testingWithRealData(int numDataSplits, int dataBatchSiz
                          .project({"transaction_id", "all_features", "softmax(mat_vector_add_3(mat_mul_3(relu(mat_vector_add_2(mat_mul_2(relu(mat_vector_add_1(mat_mul_1(all_features))))))))) AS fraudulent_probs"})
                          .filter("get_binary_class(fraudulent_probs) = 1")
                          .filter("xgboost_fraud_predict(all_features) >= 0.5")
-                         .project({"transaction_id"})
+                         .project({"transaction_id"})*/
                          .planNode();
 
 
@@ -1352,7 +1352,7 @@ void FraudDetectionTest::testingWithRealData(int numDataSplits, int dataBatchSiz
                          .localPartition({"o_customer_sk"})
                          .finalAggregation()
                          //.singleAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
-                         .hashJoin({"o_customer_sk"},
+                         /*.hashJoin({"o_customer_sk"},
                              {"t_sender"},
                              exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
                              .values(batchesTransaction)
@@ -1383,7 +1383,7 @@ void FraudDetectionTest::testingWithRealData(int numDataSplits, int dataBatchSiz
                          .project({"transaction_id", "all_features", "softmax(mat_vector_add_3(mat_mul_3(relu(mat_vector_add_2(mat_mul_2(relu(mat_vector_add_1(mat_mul_1(all_features))))))))) AS fraudulent_probs"})
                          .filter("get_binary_class(fraudulent_probs) = 1")
                          .filter("xgboost_fraud_predict(all_features) >= 0.5")
-                         .project({"transaction_id"})
+                         .project({"transaction_id"})*/
                          .planNode();
    
  
