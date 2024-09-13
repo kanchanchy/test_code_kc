a6d6ceab84415b67fd61fdafdb2bcfa8453ad7d3
kanchanchy <kanchanchy09@gmail.com>
2024-09-13 12:40:07 -0600

    'model-changed'

--- FraudDetectionTest.cpp
+++ FraudDetectionTest.cpp
@@ -1419,10 +1419,10 @@ void FraudDetectionTest::testingWithRealData(int numDataSplits, int dataBatchSiz
                          .project({"o_customer_sk", "o_order_id", "date_to_timestamp_1(o_date) AS o_timestamp"})
                          .filter("o_timestamp IS NOT NULL")
                          .filter("is_weekday(o_timestamp) = 1")
-                         .partialAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
-                         .localPartition({"o_customer_sk"})
-                         .finalAggregation()
-                         //.singleAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
+                         //.partialAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
+                         //.localPartition({"o_customer_sk"})
+                         //.finalAggregation()
+                         .singleAggregation({"o_customer_sk"}, {"count(o_order_id) as total_order", "max(o_timestamp) as o_last_order_time"})
                          .hashJoin({"o_customer_sk"},
                              {"t_sender"},
                              exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
