213e3a46a4a8e5312404cb8ddb39d03af3ea59cf
kanchanchy <kanchanchy09@gmail.com>
2024-10-15 01:50:29 -0600

    'trip-type-updated'

--- TripTypeDetectionTest.cpp
+++ TripTypeDetectionTest.cpp
@@ -1053,7 +1053,7 @@ void TripTypeDetectionTest::testingWithRealData(int numDataSplits, int dataBatch
 
      auto planNodeIdGenerator = std::make_shared<core::PlanNodeIdGenerator>();
 
-     auto myPlan1 = exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
+     /*auto myPlan1 = exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
                          .values({orderRowVector})
                          .localPartition({"o_store"})
                          .project({"o_order_id", "o_customer_sk", "o_store", "o_date", "o_weekday"})
@@ -1066,6 +1066,32 @@ void TripTypeDetectionTest::testingWithRealData(int numDataSplits, int dataBatch
                              .values({storeRowVector})
                              .localPartition({"s_store"})
                              .project({"s_store", "s_features as store_feature"})
+                             //.filter("is_popular_store(store_feature) = 1")
+                             .planNode(),
+                             "",
+                             {"o_order_id", "order_all_feature", "store_feature"}
+                         )
+                         .project({"o_order_id", "store_feature", "concat(order_all_feature, store_feature) AS all_feature"})
+                         .project({"o_order_id", "store_feature", "get_max_index(softmax(mat_vector_add_3(mat_mul_3(relu(mat_vector_add_2(mat_mul_2(relu(mat_vector_add_1(mat_mul_1(all_feature)))))))))) AS predicted_trip_type"})
+                         //.filter("o_weekday != 'Sunday'")
+                         .filter("is_popular_store(store_feature) = 1")
+                         //.project({"o_order_id", "softmax(mat_vector_add_3(mat_mul_3(relu(mat_vector_add_2(mat_mul_2(relu(mat_vector_add_1(mat_mul_1(all_feature))))))))) AS predicted_trip_type"})
+                         //.orderBy({fmt::format("{} ASC NULLS FIRST", "o_order_id")}, false)
+                         .planNode();*/
+
+     auto myPlan1 = exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
+                         .values({storeRowVector})
+                         .localPartition({"s_store"})
+                         .project({"s_store", "s_features as store_feature"})
+                         .hashJoin({"s_store"},
+                             {"o_store"},
+                             exec::test::PlanBuilder(planNodeIdGenerator, pool_.get())
+                             .values({orderRowVector})
+                             .localPartition({"o_store"})
+                             .project({"o_order_id", "o_customer_sk", "o_store", "o_date", "o_weekday"})
+                             .filter("o_weekday != 'Sunday'")
+                             .project({"o_order_id", "o_store", "customer_id_embedding(convert_int_array(o_customer_sk)) as customer_id_feature", "get_order_features(o_date, o_weekday) AS order_feature"})
+                             .project({"o_order_id", "o_store", "concat(customer_id_feature, order_feature) as order_all_feature"})
                              //.filter("is_popular_store(store_feature) = 1")
                              .planNode(),
                              "",
